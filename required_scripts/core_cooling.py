1b64d654292d0435734d29852dbd6f087aa78592
murphyqm <murphyqm@tcd.ie>
2020-09-04 17:35:09 +0100

    Lint core_cooling module
    Old modules copied across from previous repo are messy - work through and tidy code
    so that it conforms to PEP 8

--- required_scripts/core_cooling.py
+++ required_scripts/core_cooling.py
@@ -9,20 +9,50 @@ Created on Tue Feb 26 11:35:18 2019
 import numpy as np
 
 def core_freezing(coretemp, max_time, times, latent,temp_core_melting,timestep=1E11):
-    #finding time where the core starts to freeze
-    core_frozen = [coretemp<=temp_core_melting] #creates boolean array for temp<=1200
-    times_frozen = np.where(core_frozen)[2] #np.where outputs indices where temp<=1200
+    """
+    
+
+    Parameters
+    ----------
+    coretemp : TYPE
+        DESCRIPTION.
+    max_time : TYPE
+        DESCRIPTION.
+    times : TYPE
+        DESCRIPTION.
+    latent : TYPE
+        DESCRIPTION.
+    temp_core_melting : TYPE
+        DESCRIPTION.
+    timestep : TYPE, optional
+        DESCRIPTION. The default is 1E11.
+
+    Returns
+    -------
+    None.
+
+    """
+    # finding time where the core starts to freeze
+    core_frozen = [coretemp <= temp_core_melting]
+    # creates boolean array for temp<=1200
+    times_frozen = np.where(core_frozen)[2]
+    # np.where outputs indices where temp<=1200
 
     time_core_frozen = 0.0
-    if time_core_frozen>=max_time or len(times_frozen) == 0.0:
+    if time_core_frozen >= max_time or len(times_frozen) == 0.0:
         print('Core freezes after max time')
-        time_core_frozen = 0.0 #np.NaN #0
-        fully_frozen = 0.0 #np.NaN #0
+        time_core_frozen = 0.0
+        fully_frozen = 0.0
     else:
-        time_core_frozen = times_frozen[2] #first time the temperature is less than 1200K # changed to one instead of zero.... might change back
-        time_core_frozen = (time_core_frozen)*(timestep) #convert to seconds
+        time_core_frozen = times_frozen[2]
+        # first time the temperature is less than 1200K
+        # changed to 2 instead of 0
+        time_core_frozen = (time_core_frozen)*(timestep)  # convert to seconds
 
-    #find time core finishes freezing, time when latent heat is all gone + time core started to freeze
+    # find time core finishes freezing, time when latent heat is all
+    # gone + time core started to freeze
     fully_frozen = times[len(latent)] + time_core_frozen
-    print("core_frozen: "+str(core_frozen)+ "; times_frozen: "+ str(times_frozen) + "; time_core_frozen: "+str(time_core_frozen)+ "; fully_frozen: "+ str(fully_frozen))
+    print("core_frozen: " + str(core_frozen) + "; times_frozen: "
+          + str(times_frozen) + "; time_core_frozen: " + str(time_core_frozen)
+          + "; fully_frozen: " + str(fully_frozen))
     return(core_frozen, times_frozen, time_core_frozen, fully_frozen)
