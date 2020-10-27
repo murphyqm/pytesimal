c38f273ae9499a6231df6fccec433023bfc7c173
murphyqm <murphyqm@tcd.ie>
2020-10-27 17:34:39 +0000

    Add test for variable run as well

--- required_scripts/test_highlevel.py
+++ required_scripts/test_highlevel.py
@@ -35,3 +35,33 @@ def test_def_const():
             vImilac_Depth, vIm_timing) == (begins_to_freeze, finished_freezing,
                                            Esquel_Depth, Esq_timing,
                                            Imilac_Depth, Im_timing)
+
+
+def test_def_var():
+    """
+    Simple test to check default results of variable model
+
+    Runs constant model with default values and checks that these are expected.
+    High level test that only checks final output - need more granular testing.
+
+    Returns
+    -------
+    None.
+
+    """
+    (vbegins_to_freeze, vfinished_freezing, vEsquel_Depth, vEsq_timing,
+     vImilac_Depth, vIm_timing) = (6662500000000000.0, 8978400000000000.0,
+                                   68.0, 7819700000000000, 61.0,
+                                   6501800000000000)
+    run_ID = "simple_test_22oct20_v"
+    (begins_to_freeze, finished_freezing, Esquel_Depth, Esq_timing,
+     Imilac_Depth, Im_timing) = mcc.conductive_cooling(run_ID, folder,
+                                                       cond_constant="n",
+                                                       density_constant="n",
+                                                       heat_cap_constant="n",
+                                                       return_vars="y",
+                                                       save_param_file="n")
+    assert (vbegins_to_freeze, vfinished_freezing, vEsquel_Depth, vEsq_timing,
+            vImilac_Depth, vIm_timing) == (begins_to_freeze, finished_freezing,
+                                           Esquel_Depth, Esq_timing,
+                                           Imilac_Depth, Im_timing)
