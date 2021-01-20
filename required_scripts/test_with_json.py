24a7b6269141da33181e5fe03d7cf58034df9944
murphyqm <murphyqm@tcd.ie>
2021-01-20 17:58:27 +0000

    Add json files for testing

---
+++ required_scripts/test_with_json.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 20 15:02:05 2021.
+
+@author: maeve
+"""
+
+import pickle
+import modular_cond_cooling as mcc
+
+import os
+import inspect
+
+import json
+
+folder = "output_runs/auto_pytest"
+
+
+def test_core_const():
+    """
+    Simple test that loads default json files and asserts they match output.
+
+    Returns
+    -------
+    None.
+
+    """
+    run_ID = "test_python_constant"
+    mcc.conductive_cooling(run_ID, folder, return_vars="n",
+                           save_param_file="n", tests="y")
+    vals = [1, 1000, 126228]
+    folder1 = "output_runs/testing_output/"
+    folder2 = "default_test_results/"
+    DATA2 = os.path.join(os.path.dirname(os.path.abspath(
+            inspect.getfile(inspect.currentframe()))), folder2,
+            "constant_core.json")
+    with open(
+            DATA2,
+            'r',
+            ) as fp:
+        default_list = json.load(fp)
+    for i in vals:
+        string = "core_test_i_" \
+            + str(i) \
+            + "_var_" \
+            + "y" \
+            + ".pickle"
+        DATA = os.path.join(os.path.dirname(os.path.abspath(
+            inspect.getfile(inspect.currentframe()))), folder1, string)
+        with open(
+            DATA,
+            "rb",
+        ) as f:
+            [
+                latent,
+                i,
+                dr,
+                temperature_core,
+                temp_core_melting,
+                core_lh_extracted,
+                max_core_lh,
+                cmb_conductivity,
+                timestep,
+                core_density,
+                core_cp,
+                r_core,
+                label,
+            ] = pickle.load(f)
+        new_list = [
+                latent,
+                i,
+                dr,
+                temperature_core,
+                temp_core_melting,
+                core_lh_extracted,
+                max_core_lh,
+                cmb_conductivity,
+                timestep,
+                core_density,
+                core_cp,
+                r_core,
+                label,
+            ]
+
+        assert [new_list] == default_list[str(i)]
+
+
+def test_core_var():
+    """
+    Simple test that loads default json files and asserts they match output.
+
+    Returns
+    -------
+    None.
+
+    """
+    run_ID = "test_python_variable"
+    mcc.conductive_cooling(run_ID, folder, return_vars="n",
+                           save_param_file="n", tests="y")
+    vals = [1, 1000, 126228]
+    folder1 = "output_runs/testing_output/"
+    folder2 = "default_test_results/"
+    DATA2 = os.path.join(os.path.dirname(os.path.abspath(
+            inspect.getfile(inspect.currentframe()))), folder2,
+            "variable_core.json")
+    with open(
+            DATA2,
+            'r',
+            ) as fp:
+        default_list = json.load(fp)
+    for i in vals:
+        string = "core_test_i_" \
+            + str(i) \
+            + "_var_" \
+            + "n" \
+            + ".pickle"
+        DATA = os.path.join(os.path.dirname(os.path.abspath(
+            inspect.getfile(inspect.currentframe()))), folder1, string)
+        with open(
+            DATA,
+            "rb",
+        ) as f:
+            [
+                latent,
+                i,
+                dr,
+                temperature_core,
+                temp_core_melting,
+                core_lh_extracted,
+                max_core_lh,
+                cmb_conductivity,
+                timestep,
+                core_density,
+                core_cp,
+                r_core,
+                label,
+            ] = pickle.load(f)
+        new_list = [
+                latent,
+                i,
+                dr,
+                temperature_core,
+                temp_core_melting,
+                core_lh_extracted,
+                max_core_lh,
+                cmb_conductivity,
+                timestep,
+                core_density,
+                core_cp,
+                r_core,
+                label,
+            ]
+
+        assert [new_list] == default_list[str(i)]
