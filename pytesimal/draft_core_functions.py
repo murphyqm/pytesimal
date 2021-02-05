f07607a0fb70c5f309bca64a20bd2e9ec5d958d8
murphyqm <murphyqm@tcd.ie>
2021-02-05 15:16:06 +0000

    Pull cmb k out so this can be updated by timestepping function
    Create better temp array and change params to see core freezing, increase
    max latent heat so that model must pause on core freezing

--- pytesimal/draft_core_functions.py
+++ pytesimal/draft_core_functions.py
@@ -12,7 +12,7 @@ import numpy as np
 class Core:
     """Core class represents and manipulates core temp and latent heat."""
 
-    def __init__(self, temp, melt, r, rho, cp, k, maxlh, lat=0):
+    def __init__(self, temp, melt, r, rho, cp, maxlh, lat=0):
         """Create a new core with temperature and latent heat."""
         self.temperature = temp
         self.latent = lat
@@ -20,7 +20,6 @@ class Core:
         self.radius = r
         self.density = rho
         self.heatcap = cp
-        self.cmbk = k
         self.maxlatent = maxlh
         self.templist = []
         self.latentlist = []
@@ -35,13 +34,13 @@ class Core:
         self.temperature -= 100.0
         self.latent += 10.0
 
-    def cooling(self, mantletemps, timestep, dr, i):
+    def cooling(self, mantletemps, timestep, dr, i, cmbk):
         """Cool core or extract latent heat."""
         if (self.temperature > self.melting) or (self.latent >= self.maxlatent):
-            print(self.temperature)
+            # print(self.temperature)
             self.temperature = self.temperature - (
                 3.0
-                * self.cmbk
+                * cmbk
                 * ((mantletemps[0, i] - mantletemps[1, i]) / dr)
                 * timestep) / (self.density * self.heatcap * self.radius)
             self.templist.append(self.temperature)
@@ -49,34 +48,37 @@ class Core:
             self.latent = (
                 self.latent
                 + (4.0 * np.pi * self.radius ** 2)
-                * self.cmbk
+                * cmbk
                 * ((mantletemps[0, i] - mantletemps[1, i]) / dr)
                 * timestep)
             self.latentlist.append(self.latent)
+            print("Freezing!\n***\n***\n***")
 
 
 """Testing this draft Class"""
 
 # instantiate core object
 
-temp = 1800.0
+temp = 2000.0
 melt = 1200.0
 r = 100.0
 rho = 7800.0
 cp = 850.0
-k = 3.0
-maxlh = 270_000.0
+k = 5.0
+maxlh = 4_000_000_000_000_000.0
 
 
-core1 = Core(temp, melt, r, rho, cp, k, maxlh, lat=0)
+core1 = Core(temp, melt, r, rho, cp, maxlh, lat=0)
 print(core1)
 
 # set up a mantle array
 dt = 1
 dr = 1.0
 
+max_t = 35000.0
+
 radii = np.arange(100.0, 200.0, dr)
-times = np.arange(0, 100000.0, dt)
+times = np.arange(0, max_t, dt)
 
 temperatures = np.zeros((radii.size, times.size))  # shape: (100, 10000)
 
@@ -84,17 +86,20 @@ temperatures = np.zeros((radii.size, times.size))  # shape: (100, 10000)
 # Give the mantle a pretend cooling history (interactive in real application):
 
 
-temp_init = 1600.0
+temp_init = 2000.0
+dT = max_t/50000.0
+print(dT)
 
-for j in range(times.size-1, 0, -1):
-    for i in range(0, radii.size):
+for j in range(0, times.size):
+    for i in range(radii.size-1, 0, -1):
         temperatures[i, j] = temp_init
-        temp_init -= 10
+        temp_init = temp_init - dT
+        # print(temp_init)
 
 
 # make the core cool:
 
-for i in range (0, 10000, dt):
-    core1.cooling(temperatures, dt, dr, i)
-    # print(core1)
+for i in range(0, times.size, dt):
+    core1.cooling(temperatures, dt, dr, i, k)
+    print(core1)
 
