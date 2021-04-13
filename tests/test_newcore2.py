096ac8a03dee4d9a220d68e4087618924aaf9e99
murphyqm <murphyqm@tcd.ie>
2021-04-13 09:34:19 +0100

    Update unit tests for new core object

--- tests/test_newcore2.py
+++ tests/test_newcore2.py
@@ -12,33 +12,33 @@ import numpy as np
 import pytest
 
 
-def test_new_core_function_1():
-    radii = np.arange(10000.0, 50000.0, 1000.0)
-    times = np.arange(0, 100 + 0.5 * 1.0, 1.0)
-    temperatures = np.zeros((radii.size, times.size))
-    core = new_core_function_2.IsothermalEutecticCore(temp=1000.0,
-                                                      melt=1200.0,
-                                                      outer_r=10000.0,
-                                                      inner_r = 0,
-                                                      rho=7800.0,
-                                                      cp=850.0,
-                                                      #maxlh=10000.0,
-                                                      core_latent_heat=3.060671982536449e-13,
-                                                      lat=7000.0)
-    core.cooling(temperatures,
-                 timestep=1.0,
-                 dr=100.0,
-                 i=1,
-                 cmbk=3.0)
-    latent = core.latentlist
-    core_lh_extracted = core.latent
-    temperature_core = core.temperature
-    assert latent == [7000.0]
-    assert core_lh_extracted == 7000.0
-    assert temperature_core == 1000.0
-    print("Success.")
+# def test_new_core_function_1():
+#     radii = np.arange(10000.0, 50000.0, 1000.0)
+#     times = np.arange(0, 100 + 0.5 * 1.0, 1.0)
+#     temperatures = np.zeros((radii.size, times.size))
+#     core = new_core_function_2.IsothermalEutecticCore(temp=1000.0,
+#                                                       melt=1200.0,
+#                                                       outer_r=10000.0,
+#                                                       inner_r = 0,
+#                                                       rho=7800.0,
+#                                                       cp=850.0,
+#                                                       #maxlh=10000.0,
+#                                                       core_latent_heat=3.060671982536449e-13,
+#                                                       lat=7000.0)
+#     core.cooling(temperatures,
+#                  timestep=1.0,
+#                  dr=100.0,
+#                  i=1,
+#                  cmbk=3.0)
+#     latent = core.latentlist
+#     core_lh_extracted = core.latent
+#     temperature_core = core.temperature
+#     assert latent == [7000.0]
+#     assert core_lh_extracted == 7000.0
+#     assert temperature_core == 1000.0
+#     print("Success.")
 
-def test_new_core_function_2():
+def test_new_core_unit():
     radii = np.arange(10000.0, 50000.0, 1000.0)
     times = np.arange(0, 100 + 0.5 * 1.0, 1.0)
     temperatures = np.zeros((radii.size, times.size))
@@ -54,15 +54,14 @@ def test_new_core_function_2():
                                                       cp=850.0,
                                                       core_latent_heat=3.060671982536449e-13,
                                                       lat=7000.0)
-    # core.cooling(temperatures,
-    #              timestep=1.0,
-    #              dr=100.0,
-    #              i=1,
-    #              cmbk=3.0)
-    energy = cmb_energy.energy_extracted(temperatures,
-                                         i=1,
-                                         k=3.0)
-    core.extract_energy(energy)
+    # energy = cmb_energy.energy_extracted(temperatures,
+    #                                      i=1,
+    #                                      k=3.0)
+    heat = cmb_energy.power(temperatures,
+                            i=1,
+                            k=3.0)
+    core.extract_heat(heat, timestep=1.0)
+    # core.extract_energy(energy)
     latent = core.latentlist
     core_lh_extracted = core.latent
     temperature_core = core.temperature
