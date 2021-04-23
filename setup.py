137c8bd959984f583b422bf817afb5e1f379705e
murphyqm <murphyqm@tcd.ie>
2021-04-23 17:34:49 +0100

    Editing setup files for packaging

--- setup.py
+++ setup.py
@@ -1,3 +1,34 @@
-import setuptools
+from distutils.core import setup
 
-setuptools.setup()
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="pytesimal",
+    version="0.0.1",
+    description="Model the conductive cooling of planetesimals with temperature-dependent material properties.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Maeve Murphy Quinlan",
+    author_email="eememq@leeds.ac.uk",
+    url="https://github.com/murphyqm/pytesimal",
+    project_urls={
+        "Documentation": "https://pytesimal.readthedocs.io/en/latest/",
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+        "Scientific Background:": "https://doi.org/10.1029/2020JE006726"},
+    packages=["pytesimal"], # trying this, now that cfg file is removed
+    # py_modules=["pytesimal"],
+    python_requires='>=3.7',
+    install_requires=['numpy','matplotlib',],
+    setup_requires=['numpy','matplotlib'],
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Visualization",
+        "Intended Audience :: Science/Research",
+        "Natural Language :: English",
+    ],
+)
